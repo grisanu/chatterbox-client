32a20781bc41f1605efe14fa0e2f0840903a397d
mattgrisanu <mgrisanu@gmail.com>
2016-05-02 15:36:27 -0700

    Created FETCH, SEND and clearMessages methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,47 @@
 // YOUR CODE HERE:
 
+window.app = {
+  server: undefined,
+  init: function () {},
+  send: function (messageArray) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(messageArray),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function () {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get message', data);
+      }
+    });
+  },
+  addRoom: function () {},
+  clearMessages: function () {
+    // would this also remove all non-direct children?
+    $('#chats').children().remove();
+  },
+  addMessage: function () {},
+  addFriend: function () {},
+};
