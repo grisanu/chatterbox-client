2e3dd2c63ace373f80231c565db165855da261fd
mattgrisanu <mgrisanu@gmail.com>
2016-05-03 10:41:15 -0700

    New files from Sehoon

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,82 +1,81 @@
-// YOUR CODE HERE:
-
 window.app = {
-  server: undefined,
+  server: 'https://api.parse.com/1/classes/messages',
   init: function () {
-    // $('.username').on('click', function () {
-    //   $(this).addFriend();
-    // });
+    this.fetch(function(messages){
+      messages = messages.results
+
+      _.each(messages, function(message) {
+        app.addMessage(message);
+      })
+    });
     this.addFriend();
-    this.handleSubmit();
+    $('#submitForm').submit(app.handleSubmit);
   },
-  send: function (messageArray) {
+  send: function (message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/messages',
+      url: this.server,
       type: 'POST',
-      data: JSON.stringify(messageArray),
+      data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message', data);
       }
     });
   },
-  fetch: function () {
+  fetch: function (callback) {
     $.ajax({
-      // This is the url you should use to communicate with the parse API server.
       url: this.server,
       type: 'GET',
-      data: {
-        format: 'json'
-      },
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message received');
+        if (callback) {
+          callback(data);
+        }
       },
       error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to get message', data);
       }
     });
   },
-  clearMessages: function () {
-    // would this also remove all non-direct children?
-    $('#chats').children().remove();
+  handleSubmit: function (e) {
+    e.preventDefault();
+    var $newMsg = $('.messageBox').val();
+    var message = {
+      text: $newMsg,
+      username: 'Tester'
+    }
+    app.send(message)
+    $('.messageBox').val('');
+  },
+  clearMessages: function() {
+    $('#chats').empty();
   },
   addMessage: function (message) {
-    var $chat = $('<div></div>');
-
+    var $chats = $('#chats');
+    var $chat = $('<div class="chat"></div>');
+    
     var $user = $('<a href="#" class="username"></a>');
     $user.attr('data-user', message.username);
-    $user.text(message.username + ' : ');
+    $user.text(message.username + ': ');
 
-    var $msg = $('<div></div>');
-    $msg.text(message.text);
-
+    var $msg = $('<div>' + message.text + '</div>');
+    
     $chat.append($user, $msg);
-
-    $('#chats').append($chat);
+    $chats.append($chat);
   },
   addRoom: function (roomName) {
-    var $room = $('<div></div>');
-
-    $room.addClass(roomName);
-    $('#roomSelect').append($room);
+    var $roomSelect = $('#roomSelect');
+    var $room = $('<div class=' + roomName + '></div>');
+    $roomSelect.append($room);
   },
   addFriend: function () {
     $('.username').on('click', function () {
       var $this = $(this).attr('data-user');
-      console.log($this);
-    });
-  },
-  handleSubmit: function () {
-    $('#send .submit').on('submit', function() {
-      console.log($(this));
     });
   }
-
 };
+app.init();
